c7eaab22471fc32c4112238bbae1273f0e37ec59
Tukaram Gore <187551059+tr285@users.noreply.github.com>
2026-07-31 10:37:52 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.77%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n =nums.size();
+        int start =0;
+        for ( int i =0; i<n;i++){
+            if (nums[i]!=0){
+                swap(nums[start],nums[i]);
+                start+=1;
+            }
+        }
+    }
+};
