c4a256757c8b6000bb301013513ddb813e62bb92
Tukaram Gore <187551059+tr285@users.noreply.github.com>
2026-08-07 23:17:46 +0530

    Time: 1 ms (69.52%), Space: 12.5 MB (22.23%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        stack = []
+
+        for part in path.split('/'):
+            if part == "" or part == ".":
+                continue
+            elif part == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(part)
+
+        return "/" + "/".join(stack)
