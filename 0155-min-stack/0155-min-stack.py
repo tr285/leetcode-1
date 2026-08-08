9e76031f7653d245a8035b3bbcd5b7da83f996f7
Tukaram Gore <187551059+tr285@users.noreply.github.com>
2026-08-08 23:13:28 +0530

    Time: 173 ms (49.51%), Space: 24.8 MB (27.03%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,38 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, value):
+        """
+        :type value: int
+        :rtype: None
+        """
+        self.stack.append(value)
+
+        if not self.min_stack:
+            self.min_stack.append(value)
+        else:
+            self.min_stack.append(
+                min(value, self.min_stack[-1])
+            )
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min_stack[-1]
