d273fb606a0191bd758cbfe4ed90acbd99a4c6d3
Tukaram Gore <187551059+tr285@users.noreply.github.com>
2026-08-15 10:35:12 +0530

    Time: 0 ms (100%), Space: 13.9 MB (79.54%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,10 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        seen =set()
+        ans =0
         for num in nums:
-            if num in seen:
-                seen.remove(num)
-            else:
-                seen.add(num)
-        return seen.pop()
+            ans = ans^num
+        return ans
