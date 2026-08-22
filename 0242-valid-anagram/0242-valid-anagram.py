06f29c728c14322a94835e2c98a280d3a7861353
Tukaram Gore <187551059+tr285@users.noreply.github.com>
2026-08-22 08:41:56 +0530

    Time: 15 ms (86.31%), Space: 12.4 MB (98.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,5 +5,16 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        
-        return sorted(s) == sorted(t)
+        if len(s)!=len(t):
+           return False
+        freq={}
+
+        for ch in s:
+            freq[ch] =freq.get(ch,0)+1
+        for ch in t:
+            if ch not in freq:
+                return False
+            freq[ch]-=1
+            if freq[ch]<0:
+                return False
+        return True
