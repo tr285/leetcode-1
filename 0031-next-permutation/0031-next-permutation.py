f09be403dcd4efe7d7f8e31b87f14815b0b1826a
Tukaram Gore <187551059+tr285@users.noreply.github.com>
2026-09-04 23:13:22 +0530

    Time: 1 ms (7.43%), Space: 19.2 MB (67.21%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        i =n-2
+        while i>=0 and nums[i]>=nums[i+1]:
+            i-=1
+        if i==-1:
+            nums.sort()
+            return
+        j=i+1
+        while j<n and nums[j]>nums[i]:
+            j+=1
+        j-=1
+        nums[i],nums[j]=nums[j],nums[i]
+        nums[i+1:]=sorted(nums[i+1:])
+        
+
