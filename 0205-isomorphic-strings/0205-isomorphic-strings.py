5d75a7efab6cca448fbe888210227432c4c02053
Tukaram Gore <187551059+tr285@users.noreply.github.com>
2026-09-07 19:17:38 +0530

    Time: 3 ms (91.72%), Space: 19.7 MB (6.25%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,21 +1,3 @@
-class Solution(object):
-    def isIsomorphic(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        maps ={}
-        mapt ={}
-        for c1,c2 in zip(s,t):
-            if c1 in maps:
-               if maps[c1] !=c2:
-                  return False
-            else:
-                maps[c1] =c2
-            if c2 in mapt:
-                if mapt[c2] != c1:
-                    return False
-            else:
-                mapt[c2] =c1
-        return True
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return list(map(s.find, s)) == list(map(t.find, t))
